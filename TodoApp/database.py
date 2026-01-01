8817d27bd3423a5edb13aed8f9fe01f2e8bc72f1
Bhavana <holladebhavana23@gmail.com>
2026-01-01 12:44:14 +0530

    Added prod db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,9 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+#for sqlite database use this
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+#For postgresql database use this
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:bhavana%4023@localhost/TodoAppDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+#For mysql database use this
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:bhavana23@127.0.0.1:3306/todos'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
